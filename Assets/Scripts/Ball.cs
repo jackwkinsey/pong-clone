84b1cccf7aa27b2d6814ad85def55b75d110e5cd
Jack Kinsey <jack.w.kinsey@gmail.com>
2018-01-25 22:38:05 -0600

    Initial assets. Core funcitonality is implmented, but there is quite a lot of room for polish. Also, the current ball collision code is a bit messy.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 30.0f;
+
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        _rb.velocity = Vector2.right * _moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        string name = col.gameObject.name;
+        if (name == "RacketLeft" || name == "RacketRight")
+        {
+            float xDirection = 0;
+
+            if (col.gameObject.name == "RacketLeft")
+            {
+                xDirection = 1;
+            }
+            else if (col.gameObject.name == "RacketRight")
+            {
+                xDirection = -1;
+            }
+
+            float yDirection = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            Vector2 direction = new Vector2(xDirection, yDirection).normalized;
+
+            _rb.velocity = direction * _moveSpeed;
+        }
+    }
+
+    private float hitFactor(Vector2 ballPosition, Vector2 racketPosition, float racketHeight)
+    {
+        return (ballPosition.y - racketPosition.y) / racketHeight;
+    }
+}
---
+++ Assets/Scripts/MoveRacket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 30.0f;
+
+    [SerializeField]
+    private string _axis = "Vertical";
+
+    private float _yAxisInput;
+    private Vector2 _moveDirection;
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _yAxisInput = Input.GetAxisRaw(_axis);
+        _moveDirection = new Vector2(0, _yAxisInput);
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = _moveDirection * _moveSpeed;
+    }
+}
